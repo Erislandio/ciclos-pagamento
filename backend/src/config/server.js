558e3bfaa411623f54016fa4cde5afa4c4bbcc90
Erislandio Soares <erislandiosoares21@gmail.com>
2019-02-03 11:15:55 -0200

    Adição do server express

---
+++ backend/src/config/server.js
@@ -0,0 +1,12 @@
+const port = 3003
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, () => {
+    console.log(`Server esta online na porta ${port}`)
+})
